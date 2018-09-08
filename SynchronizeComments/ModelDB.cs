1e6ff944745555a91b2401a745b873100f9060cd
mdkChaos <mdkchaos1@gmail.com>
2018-09-08 21:52:47 +0200

    Created files

---
+++ SynchronizeComments/ModelDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynchronizeComments
+{
+    class ModelDB : DbContext
+    {
+        public ModelDB() : base("name=ModelDB")
+        {
+        }
+
+        public virtual DbSet<Comment> Comments { get; set; }
+    }
+}
